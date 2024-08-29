877cb2f10d0dc7205c82c6a3b4949dd7fa66afec
Ar1510 <arielst299@gmail.com>
2024-08-29 13:06:48 -0600

    Cambio

--- Exercises2.py
+++ Exercises2.py
@@ -6,6 +6,9 @@ una manera más eficaz.
  Implementar la sección de devolución la cual si la fecha excede la
 que devolución se dará una sanción"""
 
+"""La forma en la que realice este ejercicio la pense en base a que se facilitaran o simplificaran un poco los procesos
+esto tambien tomando en cuenta de que los multiples def que hemos usado, cada uno tiene un trabajo en especifico y que nos ayuda a 
+hacer de forma mas ordenada cada accion"""
 prestamosActivos = []
 
 class Prestamo:
@@ -16,7 +19,7 @@ class Prestamo:
         self.titulo = titulo
         self.fechaInicio = fechaInicio
         self.duracion = int(duracion)
-
+#Funcion que nos permite mostrar la informacion adquirida
     def mostrarInformacion(self):
         print("Usuario: ", self.usuario)
         print("Entidad: ", self.entidad)
@@ -24,14 +27,14 @@ class Prestamo:
         print("Título del Libro: ", self.titulo)
         print("Fecha de Inicio: ", self.fechaInicio)
         print("Duración (días): ", self.duracion)
-
+#Funcion que nos permite evaluar el estado del prestamos segun los dias que han pasado y comparando la duracion del prestamo que escogimos
     def evaluarDevolucion(self):
         diasPasados = int(input("Ingrese el número de días transcurridos desde el inicio del préstamo: "))
         if diasPasados > self.duracion:
             print("Se ha excedido el límite de préstamo, se aplicará una sanción.")
         else:
             print("El préstamo está dentro del período permitido para la devolución.")
-
+#Con esta funcion hacemos la creacion del prestamo, obteniendo todos los datos que se necesitan recopilar a la hora de realizar un prestamo
 def crearPrestamo():
     usuario = input("Ingrese su nombre completo: ")
     entidad = input("Ingrese su entidad: ")
